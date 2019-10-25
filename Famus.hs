0d6ee164a1fff5cb01e03a17938066168e107a52
Magnus Eld <the_proffs@yahoo.se>
2019-10-25 22:52:31 +0200

    added main loop, more rude lines for Bob, more famus pepol in famus.qa

--- Famus.hs
+++ Famus.hs
@@ -1,11 +1,12 @@
 import System.Directory
 import System.IO
+import Control.Monad
 
 data QA = Q String QA QA
         | A String
          deriving (Show, Read)
 
-startTree = Q "Is she from Europe?" (A "Marie Curie") (A "Marilyn Monroe")
+defaultQA = Q "Is she from Europe?" (A "Marie Curie") (A "Marilyn Monroe")
 
 question :: String -> IO String
 question q = do putStr (q ++ " ")
@@ -17,7 +18,7 @@ yesNoQuestion q = do
   answer <- question q
   case answer of "yes" -> return True
                  "no"  -> return False
-                 _     -> yesNoQuestion "Answer yes or no!"
+                 _     -> yesNoQuestion "Answer me yes or no!"
 
 saveQA :: QA -> IO ()
 saveQA x = writeFile path $ show x
@@ -31,7 +32,7 @@ path = "famus.qa"
 getQA :: IO QA
 getQA = do exist <- doesFileExist path
            if exist then loadQA
-           else return startTree
+             else return defaultQA
 
 main :: IO ()
 main = do
@@ -39,6 +40,10 @@ main = do
   putStrLn "Think of a famous person! I will ask you questions about her."
   qa' <- play qa
   saveQA qa'
+  again <- yesNoQuestion "Play again?"
+  if again then main
+    else putStrLn "OK! Bye pesky human!"
+
 
 play :: QA -> IO QA
 play (Q s qyes qno) = do
@@ -50,12 +55,14 @@ play (Q s qyes qno) = do
             return (Q s qyes t)
 
 play (A s) = do
-  yes <- yesNoQuestion ("is the person you're thinking about " ++ s ++ "?")
+  yes <- yesNoQuestion ("Is the person you're thinking about " ++ s ++ "?")
   if yes
-    then do putStrLn "in your face! AI is taking over!"
+    then do putStrLn "In your face! AI is taking over!"
             return (A s)
     else do name <- question "I don´t belive you! Who was it!?"
+            putStrLn ("I`ve never heard of " ++ name ++ "... " ++
+                     "I told you to think of someone famous right? But OK...")
             quest <- question ("Give me a question for which the answer for "
                               ++ name ++ " is yes and the answer for "
-                              ++ s ++ " is no.")
+                              ++ s ++ " is no.\n>")
             return (Q quest (A name) (A s))
