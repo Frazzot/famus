3a4a946d34252b5c06a04c5f54235fc700529cbc
VHaugen <vicke00@live.com>
2019-10-25 11:44:19 +0200

    main, play and getQA gjord

--- Famus.hs
+++ Famus.hs
@@ -40,18 +40,16 @@ loadQA = read <$> readFile path
 path :: FilePath
 path = "famus.qa"
 
-
-
+getQA :: IO QA
+getQA = do exist <- errorTest
+           if (exist) then loadQA
+           else return testTree
 
 main :: IO ()
-main = do exist <- errorTest
-          if (not exist)
-            then do error "file Does not exist"
-                    return () 
-           else do
-                    loadQA
-                    putStrLn ("Think of a famous person! I will ask you questions about her?!?!?!?")
-            
+main = do qa <- getQA
+          putStrLn ("Think of a famous person! I will ask you questions about her?!?!?!?")
+          qa' <- play qa 
+          writeQA qa'
 
 play :: QA -> IO QA
 play (Q s qyes qno) = do yes <- yesNoQuestion s 
@@ -59,7 +57,6 @@ play (Q s qyes qno) = do yes <- yesNoQuestion s
                                         return (Q s t qno)
                          else do t <- play qno
                                  return  (Q s qyes t) 
-
 play (A s) = do ans <- yesNoQuestion( "is the person you're thinking about " ++ s )
                 if ans then do putStrLn "in your face AI is taking over" 
                                return (A s)
