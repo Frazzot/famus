0f519c2bc7de2e64741221381b704548105b9883
Magnus Eld <the_proffs@yahoo.se>
2019-10-25 15:11:52 +0200

    Clean up and updated famos.qa

--- Famus.hs
+++ Famus.hs
@@ -1,72 +1,60 @@
---famus
---import Data.List
-import System.IO 
-import System.IO.Error
 import System.Directory
-
-
 
 data QA = Q String QA QA
         | A String
          deriving (Show, Read)
 
-testTree = Q "Is she from Europe?" (A "Marie Curie") (A "Marilyn Monroe")
-testString = show testTree
+startTree = Q "Is she from Europe?" (A "Marie Curie") (A "Marilyn Monroe")
 
 question :: String -> IO String
 question q = do putStrLn q
-                getLine  
+                getLine
 
 yesNoQuestion :: String -> IO Bool
 yesNoQuestion q = do auto <- question q
                      if (auto == "yes")
-                       then do return True
+                       then return True
                      else if (auto == "no")
-                       then do return False
-                     else do yesNoQuestion q
+                       then return False
+                     else yesNoQuestion q
 
-errorTest :: IO Bool
-errorTest = doesFileExist path
+saveQA :: QA -> IO ()
+saveQA x = writeFile path (show x)
 
-
-writeQA:: QA -> IO ()
-writeQA x = writeFile path (show x)
-            
-
-loadQA:: IO QA
+loadQA :: IO QA
 loadQA = read <$> readFile path
- 
 
 path :: FilePath
 path = "famus.qa"
 
 getQA :: IO QA
-getQA = do exist <- errorTest
-           if (exist) then loadQA
-           else return testTree
+getQA = do exist <- doesFileExist path
+           if exist then loadQA
+           else return startTree
 
 main :: IO ()
-main = do qa <- getQA
-          putStrLn ("Think of a famous person! I will ask you questions about her?!?!?!?")
-          qa' <- play qa 
-          writeQA qa'
+main = do
+  qa <- getQA
+  putStrLn ("Think of a famous person! I will ask you questions about her.")
+  qa' <- play qa
+  saveQA qa'
 
 play :: QA -> IO QA
-play (Q s qyes qno) = do yes <- yesNoQuestion s 
-                         if yes then do t <- play qyes
-                                        return (Q s t qno)
-                         else do t <- play qno
-                                 return  (Q s qyes t) 
-play (A s) = do ans <- yesNoQuestion( "is the person you're thinking about " ++ s )
-                if ans then do putStrLn "in your face AI is taking over" 
-                               return (A s)
-                else do name <- question "Just curious: Who was your famous person?" 
-                        quest <- question( "Give me a question for which the answer for " 
-                                           ++ name ++ " is yes and the answer for " ++ s ++ " is no")
-                        return (Q quest (A name) (A s))
-          
+play (Q s qyes qno) = do
+  yes <- yesNoQuestion s
+  if yes
+    then do t <- play qyes
+            return (Q s t qno)
+    else do t <- play qno
+            return (Q s qyes t)
 
-
-
-
-
+play (A s) = do
+  ans <- yesNoQuestion( "is the person you're thinking about " ++ s )
+  if ans
+    then do putStrLn "in your face AI is taking over"
+            return (A s)
+    else do name <- question "I don´t belive you! Who was it!?"
+            quest <- question ("Give me a question for which the answer for "
+                              ++ name ++ " is yes and the answer for "
+                              ++ s ++ " is no")
+            return (Q quest (A name) (A s))
